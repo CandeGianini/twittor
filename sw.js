b37ee9a613d2627d56cf419983cdc4c44e346ca5
Candela Gianini <linksagenciacreativa@gmail.com>
2022-11-14 15:46:17 -0300

    cambios ios x+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('js/sw-utils.js')
 
-const STATIC_CACHE = 'static-v4';
-const DYNAMIC_CACHE = 'dynamic-v4';
-const INMUTABLE_CACHE = 'inmutable-v4';
+const STATIC_CACHE = 'static-v5';
+const DYNAMIC_CACHE = 'dynamic-v5';
+const INMUTABLE_CACHE = 'inmutable-v5';
 
 const APP_SHELL = [
     //'/',
@@ -37,6 +37,9 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
         keys.forEach(key => {
             if(key !== STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+            if(key !== DYNAMIC_CACHE && key.includes('dynamic')){
                 return caches.delete(key);
             }
         })
